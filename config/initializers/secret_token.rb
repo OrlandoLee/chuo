8d77796fa0321860381f11ff40ed115101710535
OrlandoLee <zongshengli@ucla.edu>
2014-05-31 17:06:11 -0700

    skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chuo::Application.config.secret_key_base = '66315de92ba86fb9d0123f1405f05f2b3c0046a3a607bf3b4c81b10a72712d893dcd2977d6bd29fbe194c407da164704c143142905d34750318c02e9a727ef3c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chuo::Application.config.session_store :cookie_store, key: '_Chuo_session'
