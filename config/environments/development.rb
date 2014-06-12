52d0caad3e1e5b86d02fa7143bad338d5f1b2ce2
OrlandoLee <zongshengli@ucla.edu>
2014-06-11 20:24:43 -0700

    add email and add invitation code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,17 @@ Chuo::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  
+  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "chuochuo.me" }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'chuochuo.me',
+      user_name:            'dealfinderorlando@gmail.com',
+      password:             'csdyb5twvlbo',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,18 @@ Chuo::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "chuochuo.me" }
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'chuochuo.me',
+      user_name:            'help.chuochuo.me@gmail.com',
+      password:             'csdyb5twvlbo',
+      authentication:       'plain',
+      enable_starttls_auto: true  }
 end
+
