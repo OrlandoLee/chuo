1cfa1d662d09c3d14c46c2a4bddee15b20ff1bcb
OrlandoLee <zongshengli@ucla.edu>
2014-08-02 11:36:18 -0700

    ol - only background img for sign in page

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Chuo::Application.routes.draw do
     end
 
   end
-  
+
   devise_scope :user do
     get 'users/sign_up_business', :to => 'users/registrations#new_business'
   end
   
-  devise_for :users, :controllers => {:registrations => "users/registrations"}
+  devise_for :users, :controllers => {:registrations => "users/registrations", :sessions => "users/sessions"}
   get "display/index"
   post 'display/exchange' => 'display#exchange'
   # The priority is based upon order of creation: first created -> highest priority.
