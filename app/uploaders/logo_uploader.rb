23438b83b9f1577c66072725841d8ebd8f5e94f6
OrlandoLee <zongshengli@ucla.edu>
2014-08-04 19:45:15 -0700

    resize image

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -33,7 +33,7 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
    version :thumb do
-     process :resize_to_limit => [437, 258]
+     process :resize_to_limit => [260, 152]
    end
 
   # Add a white list of extensions which are allowed to be uploaded.
